025edec83f7ff3615c52431fcc51f8be05602d17
Xamã <xcardoso1997@gmail.com>
2024-05-16 20:48:48 -0300

    pause menu ajeitado

--- PATRO_in_Mars/scripts/Pause_Menu.gd
+++ PATRO_in_Mars/scripts/Pause_Menu.gd
@@ -1,18 +1,11 @@
 extends CanvasLayer
 
 
-@onready var continue_btn = $Menu/continue_btn
+@onready var continue_btn : Button = $Menu/continue_btn
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	visible = false
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_cancel"):
@@ -22,20 +15,10 @@ func _unhandled_input(event):
 
 
 
-
-
-
-
-
-
-
 func _on_continue_btn_pressed():
 	get_tree().paused = false
 	visible = false
 	
-	
-	
-
 
 func _on_get_out_btn_pressed():
 	get_tree().quit()
