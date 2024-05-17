3190c2cb9513ea04c9f08c9071c131744b7a7135
Enzo Melo <enzomelo333@gmail.com>
2024-05-16 22:09:20 -0300

    Adicionado o Menu principal.Revisar a fonte das palavras,imagem de fundo e centralizar as opções

---
+++ PATRO_in_Mars/scripts/main_menu.gd
@@ -0,0 +1,21 @@
+class_name MainMenu 
+extends Control
+
+@onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer2/Start_Button as Button
+
+
+@onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer2/Exit_Button as Button
+@export var start_level = preload("res://scenes/world.tscn") as PackedScene
+
+func _ready():
+	start_button.button_down.connect(on_start_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
+	
+func on_start_pressed() -> void:
+	get_tree().change_scene_to_packed(start_level)
+	
+
+
+func on_exit_pressed() -> void:
+	get_tree().quit()
+	
