1da9c745895f3620bfed3374e99c7acec89f4d1e
Enzo Melo <enzomelo333@gmail.com>
2024-05-18 20:10:10 -0300

    Criados parcialmente o sistema de jazida,de spawn dos recursos das jazidas e de descarregamento da bateria.Além disso,o nome Load Game no menu principal foi trocado por History,com o intuito do jogador clicar e ir para a cena contando sobre a lore do jogo(Avaliar se o plano de fundo dessa cena,o texto escrito em inglês e o tamanho da fonte estão bons).Problemas encontrados para a execução 100% do que foi criado: Sobre o sistema de jazida,o player está colidindo com a jazida,mas ela não quebra e,consequentemente,o recurso também não é dropado.Já em relação ao descarregamento da bateria,o descarregamento não é visualmente demarcado na tela de jogo,mesmo que o código aparentemente esteja correto. Opinião: O código de desc.da bateria está no final do script do player,checar se faz sentido e reparar possíveis erros. OBS.1: Sobre a cena da History no menu principal,há um botão nela responsável por voltar para o Main Menu,porém os comandos New Game e Exit não funcionam após voltar para o Main Menu e a solução que eu usei também não funcionou(Resolvi não deixar essa solução no código) OBS.2: Tive que fazer a cena da History 3 vezes porque toda vez que eu salvava tudo,fechava a godot e abira de novo ela corrompia.então verifica se ela continua corrompida. OBS.3: A jazida está na resource_rigid.tscn

---
+++ PATRO_in_Mars/scripts/deposit_box.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+const deposit_pieces = preload("res://scenes/deposit_pieces.tscn")
+const resource_instance = preload("res://scenes/resource_rigid.tscn")
+
+@onready var animation_player = $AnimationPlayer  as AnimationPlayer
+@onready var spawn_resource = $"spawn-resource" as Marker2D
+@export var pieces : PackedStringArray
+@export var hitpoints = 2
+var impulse = 50
+
+#Verificando varredura do array e instanciando pedaços
+func break_sprite():
+	for piece in pieces.size():
+		var piece_instance = deposit_pieces.instantiate()
+		get_parent().add_child(piece_instance)
+		piece_instance.get_node("texture").texture = load(pieces[piece])
+		piece_instance.global_position = global_position
+		piece_instance.apply_impulse(Vector2(randi_range(-impulse, impulse), randi_range(-impulse, -impulse * 2)))
+	queue_free()
+		
+func create_resource():
+	var resource = resource_instance.instantiate()
+	get_parent().call_deferred("add_child", resource)
+	resource.global_position = spawn_resource.global_position #A posição do marker2d vai ser onde o recurso será instanciado
+	resource.apply_impulse(Vector2(randi_range(-50, 50), -150)) #Aplicação do impulse também feito na func acima.
